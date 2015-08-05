073cfecff85935d93026d670b16c26cfe8243068
Bansha Tang <tsq1020@gmail.com>
2015-08-05 16:01:17 +0800

    Auto saves the fail-to-unsubscribe emails to a txt

--- url.py
+++ url.py
@@ -2,30 +2,33 @@
 
 import urllib2
 import time
-error_list = []
 
-def openUrl(url, index):
-    global error_list
+
+def openUrl(url, index, email):
     print 'Open Url', index, url
     try:
         ret = urllib2.Request(url)
         data = urllib2.urlopen(ret).read()
     except Exception as e:
-        error_list.append(e)
         print 'Failed with open url', e
-        
+        f = open('error_list.txt', 'a')
+        f.write(email)
+        f.close()
+
 
 def main():
     fread = open('unsubscribe.txt', 'r')
-    line = "http://www.epubor.com/codes/subscript.aspx?Action=Unsubscribe&Type=Order&Mail=" + fread.readline()
-    openUrl(line, 1)
+    url = "http://www.epubor.com/codes/subscript.aspx?Action=Unsubscribe&Type=Order&Mail="
+    email = fread.readline()
+    line = url + email
+    openUrl(line, 1, email)
     index = 1
     while line:
         index += 1
-        time.sleep(5)
-        line = "http://www.epubor.com/codes/subscript.aspx?Action=Unsubscribe&Type=Order&Mail=" + fread.readline()
-        openUrl(line, index)
-
+        time.sleep(2)
+        email = fread.readline()
+        line = url + email
+        openUrl(line, index, email)
 
 if __name__ == '__main__':
     main()
